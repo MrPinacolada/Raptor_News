e8a57081c75db3d7b45f5bb6f2164c3b8e8bec9c
BOB <vasiasko112@gmail.com>
2023-03-28 12:14:07 +0300

    none

---
+++ src/pages/about.page.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt,
+    explicabo?
+  </div>
+</template>
+
+<script lang="ts">
+export const title = "SSR";
+</script>
+
+<style scoped></style>
---
+++ src/pages/index.page.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt,
+    explicabo?
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped></style>
